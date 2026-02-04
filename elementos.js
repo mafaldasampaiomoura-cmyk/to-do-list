8ff6c53e5349bd5fb8c312787930e176c96a57b1
Mafalda Moura <mafaldamoura@Mac.lan>
2026-02-04 22:45:29 +0000

    closing all details

--- elementos.js
+++ elementos.js
@@ -1,26 +1,44 @@
-const inputBox = document.getElementById("input-box");
-const listaContainer = document.getElementById("lista-container");
+const button = document.querySelector(".button-add-task");
+const input = document.querySelector(".input-task");
+const listaCompleta = document.querySelector('.list-tasks');
+let minhaListaDeItens = []; 
 
 function adicionarTarefa(){
-    if (inputBox.value === ""){ //caso a caixa onde escrevo esteja vazia... eu faço um alerta para que a caixa não siga vazia 
-        alert("Por favor, escreva algo!")
-    }else {
-        let li = document.createElement("li"); // crio um li de forma a que crie todos os elementos a partir daí, que vou adicionar à minha lista 
+    minhaListaDeItens.push({
+        tarefa: input.value, 
+        concluida: false
+    })
 
-        li.innerHTML = inputBox.value; 
-        listaContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7"; // "\u00d7 significa que ele vai criar a cruz no final do to-do-list" O span é um markup que serve para 
-        li.appendChild(span);
-    }
-    inputBox.value="";
+    input.value = '';
+
+    mostrarTarefa();
+
 }
 
-listaContainer.onclick = function(e) { //(e) corresponde a um evento. Ou seja para ele saber que, efetivamente, houve um clique. 
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        
-    } else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-    }
-};
+function mostrarTarefa(){
+    let novaLi = '';
+
+    minhaListaDeItens.forEach((item, index) => {
+        novaLi = novaLi + `
+        <li class = "task ${item.concluida && "done"} ">
+            <img src="./Imagens_a_usar/check.png" alt="check na tarefa" onclick="tarefaConcluida(${index})">
+            <p> ${item.tarefa} </p>
+            <img src="./Imagens_a_usar/delete.png" alt= "tarefa eliminada!" onclick="eliminarTarefa(${index})">
+        </li>`
+    })
+
+    listaCompleta.innerHTML=novaLi
+}
+
+function tarefaConcluida (index){
+    minhaListaDeItens[index].concluida = !minhaListaDeItens[index].concluida
+    mostrarTarefa()
+}
+
+function eliminarTarefa(index){
+    minhaListaDeItens.splice(index, 1);
+    mostrarTarefa();
+}
+
+
+button.addEventListener('click', adicionarTarefa);
